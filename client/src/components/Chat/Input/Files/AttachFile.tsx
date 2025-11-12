b6863d5b6939c06a5977cce4747798e5c45f752a
digbu <digbu710@gmail.com>
2025-11-12 23:51:28 +0900

    Import frontend, backend from other branch
    기본 에셋에서 react를 연습한 후, 실제 제작을 위해서 타 브랜치에서 실질적으로 임포트함

--- client/src/components/Chat/Input/Files/AttachFile.tsx
+++ client/src/components/Chat/Input/Files/AttachFile.tsx
@@ -4,7 +4,6 @@ import { AttachmentIcon } from '~/components/svg';
 import { useLocalize } from '~/hooks';
 import { cn } from '~/utils';
 
-
 const AttachFile = ({
   isRTL,
   disabled,
@@ -18,7 +17,7 @@ const AttachFile = ({
   const inputRef = useRef<HTMLInputElement>(null);
   const isUploadDisabled = disabled ?? false;
 
-  return null /*(
+  return (
     <FileUpload ref={inputRef} handleFileChange={handleFileChange}>
       <TooltipAnchor
         role="button"
@@ -51,8 +50,8 @@ const AttachFile = ({
           <AttachmentIcon />
         </div>
       </TooltipAnchor>
-    </FileUpload>  
-  )*/;
+    </FileUpload>
+  );
 };
 
 export default React.memo(AttachFile);
